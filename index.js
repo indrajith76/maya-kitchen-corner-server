ed1cd92206d819c5d78fc78a1a7d7a71a5b85d3b
indrajith76 <indrajithgoswami@gmail.com>
2022-11-08 23:15:03 +0600

    configure mongodb

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+require("dotenv").config();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0iqyqsv.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    const servicesCollection = client.db("maya-kitchen-corner").collection("services");
+    
+    app.get("/home/services", async (req, res) => {
+        const query = {}
+        const services = await servicesCollection.find(query).limit(3).toArray()
+        res.send(services);
+    });
+
+  } finally {
+  }
+}
+
+run().catch((err) => console.error(err));
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Maya's Kitchen server!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});

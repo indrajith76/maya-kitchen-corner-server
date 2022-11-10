a87f4c70fddab6d469b0219efb78b083887f2799
indrajith76 <indrajithgoswami@gmail.com>
2022-11-10 09:09:03 +0600

    added review delete api

--- index.js
+++ index.js
@@ -96,6 +96,13 @@ async function run() {
       );
       res.send(result);
     });
+
+    app.delete("/myreview/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await reviewsCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 }
